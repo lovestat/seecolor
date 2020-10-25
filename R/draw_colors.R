3a46937edfa1c21f9cfe8135a796c4897f802cc9
Shangchen Song <shangchen.song@rutgers.edu>
2020-10-25 17:44:25 -0400

    Add one ribbon feature

--- R/draw_colors.R
+++ R/draw_colors.R
@@ -11,3 +11,20 @@ draw_color <- function(bg.color, text.color = "ivory", blank.len = 20 - nchar(bg
 draw_colors <- function(colors){
   purrr::walk(colors, draw_color)
 }
+
+
+draw_color_oneribbon <- function(bg.color, text.color = "ivory", blank.len = 1, next.line = FALSE){
+  tc <- make_style(text.color)
+  bgc <- make_style(bg.color, bg = T)
+  dc <- combine_styles(tc, bgc)
+  cat(
+    dc(paste0(paste0(rep(" ", blank.len), collapse = "")))
+  )
+  if (next.line) cat("\n")
+}
+
+
+draw_colors_oneribbon <- function(colors){
+  purrr::walk(colors, draw_color_oneribbon)
+}
+
--- R/show_color.R
+++ R/show_color.R
@@ -1,9 +1,10 @@
 ##' @importFrom crayon make_style combine_styles
 ##' @export
 
-show_color <- function() {
+show_color <- function(type = "ribbons") {
   previous.colors <- 0
-  show_color_while(previous.colors)
+  if (type == "ribbons")   show_color_while(previous.colors)
+  if (type == "oneribbon") show_color_oneribbon(previous.colors)
 }
 
 
@@ -16,6 +17,21 @@ show_color_while <- function(previous.colors) {
     diff.colors <- dplyr::setdiff(detected.colors, previous.colors)
     if( !purrr::is_empty(diff.colors)  && !purrr::is_empty(detected.colors)) {
       draw_colors(diff.colors)
+    }
+    previous.colors <- detected.colors
+  }
+}
+
+
+show_color_oneribbon <- function(previous.colors) {
+  message("Press esc twice to stop")
+
+  while (T) {
+    Sys.sleep(0.3)
+    detected.colors <- detect_colors()
+    diff.colors <- dplyr::setdiff(detected.colors, previous.colors)
+    if( !purrr::is_empty(diff.colors)  && !purrr::is_empty(detected.colors)) {
+      draw_colors_oneribbon(diff.colors)
     }
     previous.colors <- detected.colors
   }
