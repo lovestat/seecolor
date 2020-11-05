66010d8548705dc0f5099ca3e1a4014fd8036a98
Shangchen Song <shangchen.song@rutgers.edu>
2020-11-05 15:09:17 -0500

    Add RStudio addin

--- R/addins.R
+++ R/addins.R
@@ -1,7 +1,24 @@
+##' Pick Color as Mosaic
+##'
+##' Run `pick_color(type = 'mosaic')` as addin, you may not run
+##' this directly by code, since it has not arguments
+##'
+##'
+##' @export
+##' @md
+
 pick_color_addin_mosaic <- function(){
   seecolor::pick_color(type = 'mosaic')
 }
 
+##' Pick Color as Ribbons
+##'
+##' Run `pick_color(type = 'ribbon')` as addin, you may not run
+##' this directly by code, since it has not arguments
+##'
+##' @export
+##'
+##' @md
 pick_color_addin_ribbon <- function(){
   seecolor::pick_color(type = "ribbon")
 }
